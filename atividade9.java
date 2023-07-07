3748c273a60bf8d5a2d1ad782c1b8194e4c86a07
JoaoSzyndrowski <138259447+JoaoSzyndrowski@users.noreply.github.com>
2023-07-07 19:04:33 -0300

    Add files via upload

---
+++ atividade9.java
@@ -0,0 +1,13 @@
+public class atividade9 {
+    public static void main(String[] args) {
+        
+          System.out.println("     *       *");
+        System.out.println("   *   *    *  *");
+        System.out.println(" *       * *     *");
+        System.out.println(" *        *      *");
+        System.out.println("   *           *");
+        System.out.println("     *       *");
+        System.out.println("       *   *");
+        System.out.println("         *");
+    }
+}
